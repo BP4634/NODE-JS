4a653d1cf276a75971bcd7d7f60ecdf71c63825f
BP4634 <123105474+BP4634@users.noreply.github.com>
2023-11-21 16:58:43 -0400

    Create First-App.js

---
+++ First-App.js
@@ -0,0 +1,15 @@
+const comp = (num) => {
+
+if (num > 10) {
+  console.log("The amount is more than 10")
+
+} else if (num < 10){
+  console.log("The amount is less than 10")
+} else {
+  console.log("The amount is 10")
+}
+}
+
+comp(15)
+comp(2)
+comp(10)
