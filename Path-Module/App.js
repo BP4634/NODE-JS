c43182a3b8ef6e2b04632b031d8d21f374f7a56f
BP4634 <123105474+BP4634@users.noreply.github.com>
2023-11-23 13:53:43 -0400

    Add files via upload

---
+++ Path-Module/App.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+
+console.log(path.sep)
+
+console.log(path.join('JAVASCRIPT','PROJECT'))
+
+console.log(path.basename(path.join('JAVASCRIPT','PROJECT','node_modules')))
+
+console.log(path.resolve('JAVASCRIPT','PROJECT'))
+
